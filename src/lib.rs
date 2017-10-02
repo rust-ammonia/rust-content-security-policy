b874576b18ee1957f9d138446c578b682b418229
Michael Howell <michael@notriddle.com>
2017-10-01 22:44:06 -0700

    Add more test cases

--- src/lib.rs
+++ src/lib.rs
@@ -19,7 +19,21 @@ mod test{
         assert!(parse::parse_SourceExpression(":").is_err());
         assert!(parse::parse_SourceExpression("1:").is_err());
         assert!(parse::parse_SourceExpression("::").is_err());
+        assert!(parse::parse_SourceExpression("http").is_err());
         // TODO: figure out how to make lalrpop throw an error on unexpected trailing characters.
         //assert!(parse::parse_SourceExpression("htt:p").is_err());
+    }
+    #[test]
+    fn parse_keyword_source() {
+        assert_eq!(parse::parse_SourceExpression("'self'").unwrap(), types::Source::Self_);
+        assert_eq!(parse::parse_SourceExpression("'unsafe-inline'").unwrap(), types::Source::UnsafeInline);
+        assert_eq!(parse::parse_SourceExpression("'unsafe-eval'").unwrap(), types::Source::UnsafeEval);
+        assert_eq!(parse::parse_SourceExpression("'strict-dynamic'").unwrap(), types::Source::StrictDynamic);
+        assert_eq!(parse::parse_SourceExpression("'unsafe-hashed-attributes'").unwrap(), types::Source::UnsafeHashedAttributes);
+        assert!(parse::parse_SourceExpression("'arbitrary'").is_err());
+        assert!(parse::parse_SourceExpression("arbitrary").is_err());
+        assert!(parse::parse_SourceExpression("self").is_err());
+        // TODO: figure out how to make lalrpop throw an error on unexpected trailing characters.
+        //assert!(parse::parse_SourceExpression("'self'arbitrary").is_err());
     }
 }
