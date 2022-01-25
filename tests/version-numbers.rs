1bace5ab7a7b27f5c4f0edb2fffb85b6a3327037
Michael Howell <michael@notriddle.com>
2022-01-24 18:17:11 -0700

    Fix busted version numbers test

--- tests/version-numbers.rs
+++ tests/version-numbers.rs
@@ -1,9 +1,13 @@
-#[cfg(feature = "version_sync")]
-#[macro_use]
-extern crate version_sync;
-
-#[cfg(feature = "version_sync")]
+#[cfg(feature = "version-sync")]
 #[test]
 fn test_readme_deps() {
+    use version_sync::assert_markdown_deps_updated;
     assert_markdown_deps_updated!("README.md");
+}
+
+#[cfg(not(feature = "version-sync"))]
+#[test]
+#[ignore]
+fn test_readme_deps() {
+    assert!(false)
 }
