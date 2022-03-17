5e693821899b30aa4fd4da0bf3aeec0c46e167cf
Michael Howell <michael@notriddle.com>
2022-03-16 17:34:11 -0700

    Allow getting the document's sandboxing flag set

--- tests/sandbox.rs
+++ tests/sandbox.rs
@@ -1,5 +1,6 @@
 extern crate content_security_policy;
 use content_security_policy::*;
+use content_security_policy::sandboxing_directive::SandboxingFlagSet;
 #[test]
 fn sandbox_test_block() {
     let csp_list = CspList::parse("sandbox", PolicySource::Header, PolicyDisposition::Enforce);
@@ -88,4 +89,20 @@ fn sandbox_test_allow_images() {
         }
     );
     assert_eq!(check_result, CheckResult::Allowed);
+}
+
+#[test]
+fn sandbox_document_flags() {
+    let policy = CspList::parse("sandbox", PolicySource::Header, PolicyDisposition::Enforce);
+    assert_eq!(Some(SandboxingFlagSet::all()), policy.get_sandboxing_flag_set_for_document());
+    let policy = CspList::parse("sandbox allow-popups", PolicySource::Header, PolicyDisposition::Enforce);
+    assert_eq!(Some(SandboxingFlagSet::all() ^ SandboxingFlagSet::SANDBOXED_AUXILIARY_NAVIGATION_BROWSING_CONTEXT_FLAG), policy.get_sandboxing_flag_set_for_document());
+    let policy = CspList::parse("sandbox allow-forms", PolicySource::Header, PolicyDisposition::Enforce);
+    assert_eq!(Some(SandboxingFlagSet::all() ^ SandboxingFlagSet::SANDBOXED_FORMS_BROWSING_CONTEXT_FLAG), policy.get_sandboxing_flag_set_for_document());
+    let policy = CspList::parse("sandbox; connect-src https://*.notriddle.com:443", PolicySource::Header, PolicyDisposition::Enforce);
+    assert_eq!(Some(SandboxingFlagSet::all()), policy.get_sandboxing_flag_set_for_document());
+    let policy = CspList::parse("sandbox allow-popups; connect-src https://*.notriddle.com:443", PolicySource::Header, PolicyDisposition::Enforce);
+    assert_eq!(Some(SandboxingFlagSet::all() ^ SandboxingFlagSet::SANDBOXED_AUXILIARY_NAVIGATION_BROWSING_CONTEXT_FLAG), policy.get_sandboxing_flag_set_for_document());
+    let policy = CspList::parse("connect-src https://*.notriddle.com:443", PolicySource::Header, PolicyDisposition::Enforce);
+    assert_eq!(None, policy.get_sandboxing_flag_set_for_document());
 }
